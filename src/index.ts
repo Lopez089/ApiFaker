66d5f22c9dd68f6a78cb9361f12c7816047a56a7
Juan <aragonlopezjuan87@gmail.com>
2020-06-28 15:34:18 +0200

    aosod

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import router from './routes/jobsRouter';
 const app = express();
 
 //setting
@@ -8,6 +9,9 @@ app.set('port', process.env.PORT || 3000);
 // middleware
 app.use(morgan('dev'));
 app.use(express.json());
+
+//Router
+app.use('/api/jobs', router);
 
 app.listen(app.get('port'), () => {
     console.log(`server on port ${app.get('port')}`);
